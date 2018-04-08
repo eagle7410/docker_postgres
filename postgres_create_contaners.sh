e2ead04b3253e5ae95c1a43e08384a8a3e07454d
Igor <verycooleagle@gmail.com>
2018-04-08 14:49:33 +0300

    Open postgist and postgrest posrts.

--- postgres_create_contaners.sh
+++ postgres_create_contaners.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker run --name postgist -e POSTGRES_PASSWORD=postgres -d mdillon/postgis
-docker run --name postgres --link postgist:postgres -e POSTGRES_PASSWORD=postgres -d postgres
+docker run --name  postgist -p 5434:5432 -e POSTGRES_PASSWORD=postgres -d mdillon/postgis
+docker run --name  postgres -p 5433:5432 --link postgist:postgres -e POSTGRES_PASSWORD=postgres -d postgres
 # host postgis
-docker run --name pgadmin -p 5050:5050 -e "DEFAULT_USER=pgadmin4eagle@pgadmin.org" -e "DEFAULT_PASSWORD=pg1234" --link postgist:postgist  --link postgres:postgres -d fenglc/pgadmin4
+docker run --name pgadmin -p 5051:5050 -e "DEFAULT_USER=pg4@pg.org" -e "DEFAULT_PASSWORD=pg1234" --link postgist:postgist  --link postgres:postgres -d fenglc/pgadmin4
 
 
 # For include postgist use pgsql
